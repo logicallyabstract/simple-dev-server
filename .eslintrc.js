0fe02740b6a1e6dae8c8ef1894b099d8b7ee3d38
AJ Klein <2043439+silenceisgolden@users.noreply.github.com>
2020-06-01 01:19:06 -0500

    Add the simple dev server logic
    - CLI
    - Transpile with typescript
    - Node resolve and rewrite imports
    - Project setup

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  extends: ['airbnb-typescript/base', 'prettier'],
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  rules: {
+    'import/prefer-default-export': 'off',
+    'import/no-default-export': ['error'],
+    'spaced-comment': 'off',
+  },
+  overrides: [
+    {
+      files: ['fixtures/static/**', '*.test.ts'],
+      rules: {
+        // do not require test/fixture imports to be in main deps
+        'import/no-extraneous-dependencies': 'off',
+      },
+    },
+  ],
+};
